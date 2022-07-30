1af10acdefa637ed12f68edbfe04b292bc2fab96
Jonny Hall <car87maniac@googlemail.com>
2022-07-30 11:09:05 +0100

    Working out user inputs

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let displayOutput = 0
+displayOutput = 7
+const theMath = document.getElementById("display").value = displayOutput;
+
+
+document.getElementById("seven")
+
+let numSeven = parseInt
