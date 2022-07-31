1e37f849cb94170ad85a69aa807f617462a0a02e
Jonny Hall <car87maniac@googlemail.com>
2022-07-31 15:49:51 +0100

    Trying to log button values to console

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ displayOutput = 7
 const theMath = document.getElementById("display").value = displayOutput;
 
 
-document.getElementById("seven")
 
-let numSeven = parseInt
+
+let buttonOne = document.getElementById("one").textContent;
+let buttonTwo = document.getElementById("two").textContent;
+let buttonThree = document.getElementById("three").textContent;
+let buttonFour = document.getElementById("four").textContent;
+let buttonFive = document.getElementById("five").textContent;
+let buttonSix = document.getElementById("six").textContent;
+let buttonSeven = document.getElementById("seven").textContent;
+let buttonEight = document.getElementById("seven").textContent;
+let buttonNine = document.getElementById("seven").textContent;
+
+//addEventListener("click", output, true )
+
+function output() {
+console.log(price.value)
+}
+
+let price = document.getElementById(buttonNine);
+price.addEventListener("click", output, true)
+
+
