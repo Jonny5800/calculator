3c76dd1985c15f9b06cd344e3094ee6c4cfcc6d2
Jonny Hall <car87maniac@googlemail.com>
2022-08-09 21:54:06 +0100

    Buttons Working

--- script.js
+++ script.js
@@ -1,4 +1,81 @@
+let currentValue = "";
 
+//All DOM elements
+const buttonZero = document.getElementById("zero");
+const buttonOne = document.getElementById("one"); //buttonOne will log the button value
+const buttonTwo = document.getElementById("two");
+const buttonThree = document.getElementById("three");
+const buttonFour = document.getElementById("four");
+const buttonFive = document.getElementById("five");
+const buttonSix = document.getElementById("six");
+const buttonSeven = document.getElementById("seven");
+const buttonEight = document.getElementById("eight");
+const buttonNine = document.getElementById("nine");
+const buttonClear = document.getElementById("clear");
+const buttonMultiply = document.getElementById("multiply");
+const buttonMinus = document.getElementById("minus");
+const buttonPlus = document.getElementById("plus");
+const buttonEquals = document.getElementById("equals");
+const buttonDivide = document.getElementById("divide");
+const display = document.getElementById("display");
+
+function updateDisplay(displayValue) {
+  display.value = currentValue + displayValue;
+}
+
+//Eventlisteners
+const numberElements = [
+  buttonOne,
+  buttonTwo,
+  buttonThree,
+  buttonFour,
+  buttonFive,
+  buttonSix,
+  buttonSeven,
+  buttonEight,
+  buttonNine,
+  buttonZero,
+];
+
+//for (i = 0; i< numberElements.length ; i++){
+//numberElements[i].addEventListener
+//}
+numberElements.forEach((ref) => {
+  ref.addEventListener("click", (elem) => {
+    updateDisplay(elem.target.textContent);
+    currentValue = currentValue + elem.target.textContent;
+  });
+});
+
+buttonMultiply.addEventListener("click", () => {
+  console.log("multiply");
+});
+buttonMinus.addEventListener("click", () => {
+  console.log("Minus");
+});
+buttonPlus.addEventListener("click", () => console.log("plus"));
+buttonEquals.addEventListener("click", () => {
+  console.log("equals");
+});
+buttonDivide.addEventListener("click", () => {
+  console.log("divide");
+});
+buttonClear.addEventListener("click", () => {
+  console.log("clear");
+});
+/*
+buttonOne.addEventListener("click", (elem) => {
+  console.log(elem.target.textContent);
+  updateDisplay(1);
+});
+buttonTwo.addEventListener("click", (elem) => {
+  console.log(elem.target.textContent);
+  updateDisplay(2);
+});
+buttonThree.addEventListener("click", () => {
+  updateDisplay(3);
+});
+*/
 // This is the display
 
 /*function showDisplay(){
@@ -6,10 +83,6 @@
     }*/
 
 //Original display from and to DOM
-displayOutput = ""
-displayOutput.textContent = "" //parseInt()
-document.getElementById("display").value = displayOutput;
-
 
 /*
 //THIS SEEMS TO BE THE STICKING POINT - cannot get anything to show up in the cals screen **edit - solved above by display output**
@@ -29,26 +102,6 @@ console.log(buttonEight); //logs digit 8
 })
 */
 
-let pressEight = document.querySelector("#eight");
-pressEight.addEventListener("click", ()=>{displayOutput  =buttonEight ; 
-console.log(pressEight); //logs the HTML for 8
-//console.log(buttonEight); //logs digit 8
-return pressEight = buttonEight
-
-})
-
-
-const obj = {
-    someValue: 100,
-    someFunc: function () {
-        return this.someValue;
-    },
-};
-
-
-let getValue = obj.someFunc.bind(obj);
-console.log(getValue())
-
 //So far only managed to log the number 8 and the contents of the 8 button HTML
 
 // Methods tried in the Eventlistener function
@@ -59,19 +112,6 @@ displayOutput.textContent = buttonEight
 
 */
 //console.log(document.getElementById("display").value + pressEight.textContent)
-
-//buton values
-let buttonOne = document.getElementById("one").textContent; //buttonOne will log the button value
-let buttonTwo = document.getElementById("two").textContent;
-let buttonThree = document.getElementById("three").textContent;
-let buttonFour = document.getElementById("four").textContent;
-let buttonFive = document.getElementById("five").textContent;
-let buttonSix = document.getElementById("six").textContent;
-let buttonSeven = document.getElementById("seven").textContent;
-let buttonEight = document.getElementById("eight").textContent;
-let buttonNine = document.getElementById("nine").textContent;
-
-
 
 //console.log(buttonNine)
 //console.log(price) ****************
@@ -85,13 +125,6 @@ console.log(valueStore)
 //let pressAny = document.querySelectorAll("buttons");
 //pressAny.addEventListener("click", ()=>{console.log(24)})
 
-let sumStore = ""
-console.log(sumStore)
-/***************************** */
-let pressSeven = document.querySelector("#seven");
-pressSeven.addEventListener("click", function execute(){
-    console.log(buttonSeven.concat(sumStore))
-})
 /********************************************* */
 //console.log(sumStore.concat(buttonSeven))
 //console.log(pressSeven)
@@ -102,20 +135,6 @@ pressEight.addEventListener("click", ()=>{console.log(document.getElementById("d
 */
 
 /*************************************************** */
-
-
-let pressNine = document.querySelector("#nine"); //PressNine will log the whole div
-pressNine.addEventListener("click",()=>{console.log(9)} )
-
-let pressMultiply = document.querySelector("#multiply");
-pressMultiply.addEventListener("click", ()=>{console.log("*")})
-
-//display output not a function. 
-//create a function for the display and try this 
-let pressFour = document.querySelector("#four");
-pressFour.addEventListener("click", ()=>{
-    displayOutput.appendNumber(pressFour.innerText)
-})
 
 //Evolution of getting the five button to work >>>>>>>
 
@@ -200,78 +219,18 @@ pressFive.addEventListener("click", fiveFunc =>{
     displayOutput.textContent = key5Value
 
 })*/
-//VERSION 6
-let pressFive = document.querySelector("#five");
-pressFive.addEventListener("click", fiveFunc =>{
-    //console.log(fiveFunc.target) //logs the 5 button HTML element
-    if (!fiveFunc.target.closest("button"))return //randomWordNotButtons
-
-    let key5 = fiveFunc.target;
-    //console.log(key5.textContent) Logs the digit 5
-    let key5Value = key5.textContent
-    //console.log(key5Value) Also logs the digit 5
-
-    let = displayVal = display.textContent //Not sure how JS picks up the DISPLAY element with no other reference to it
-    //console.log(displayVal) doesnt log anything
-
-    display.textContent = 40 //key5Value
-    //console.log(displayVal) //logs 40 while display.textContent is set to 40
-
-    console.log(key5Value)
-    displayOutput = key5Value
-
-    //Possibly mkae use of  **.this** to make displayOutput available outside of the pressFive function
-    
-    //Original display from and to DOM
-    displayOutput = ""
-    displayOutput.textContent = "" //parseInt()
-    document.getElementById("display").value = displayOutput; //"original display" copy pasted inside the function for the scope of displayOutput(didnt work)
-})
-
-let pressSix = document.querySelector("#six");
-pressSix.addEventListener("click", ()=>{console.log(6)})
-
-let pressMinus = document.querySelector("#minus");
-pressMinus.addEventListener("click", ()=>{console.log("-")})
-
-let pressOne = document.querySelector("#one");
-pressOne.addEventListener("click", ()=>{console.log(1)})
-
-let pressTwo = document.querySelector("#two");
-pressTwo.addEventListener("click", ()=>{console.log(2)})
-
-let pressThree = document.querySelector("#three");
-pressThree.addEventListener("click", ()=>{return pressThree})
-
-let pressPlus = document.querySelector("#plus");
-pressPlus.addEventListener("click", ()=>{console.log("+")})
-
-let pressEquals = document.querySelector("#equals");
-pressEquals.addEventListener("click", ()=>{console.log("=")})
-
-let pressZero = document.querySelector("#zero");
-pressZero.addEventListener("click", ()=>{console.log(0)})
-
-let pressDelete = document.querySelector("#del");
-pressDelete.addEventListener("click", ()=>{console.log(0)})
 
 //Digit logs to console above
-
-
-function buttonPresses(pressThree){
-    console.log("five was pressed")}
 
 /*
 let buttonInput = document.querySelector(".buttons");
 buttonInput.addEventListener("click", ()=> {console.log(buttonInput)})
 */
-let calcButton = document.getElementsByClassName("buttons");
+
 //calcButton.addEventListener("click", ()=>{console.log(calcButton)})
 
 //console.log(pressZero) ....Logs the div for zero
 //console.log(buttonNine)
-
-
 
 //Placed at the bottom instead of deleting completely
 /*
